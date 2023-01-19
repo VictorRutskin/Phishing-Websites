878d5c4d76f02393b80ff6ce54c25c2520cfd0a6
Victor Rutskin <Lashaka250375@gmail.com>
2023-01-19 22:01:35 +0200

    USA GOV upload

---
+++ USA government/USA Government/js/session-expire-session-d2921e72.js
@@ -0,0 +1,1 @@
+(()=>{const e=document.getElementById("js-expire-session");if(e&&e.dataset.sessionTimeoutIn){const t=1e3*parseInt(e.dataset.sessionTimeoutIn,10),s=e.dataset.timeoutRefreshPath||"";setTimeout((()=>{document.location.href=s}),t)}})();
